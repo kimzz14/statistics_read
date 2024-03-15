387cdc6325625dd42e38ffc684c20dfdf15fcc05
kimzz14 <kimzz1004@gmail.com>
2024-03-15 02:05:44 +0000

    init

---
+++ statistics_read.py
@@ -0,0 +1,101 @@
+from optparse import OptionParser
+import sys, gzip
+
+#option parser
+parser = OptionParser(usage="""Run annotation.py \n Usage: %prog [options]""")
+parser.add_option("-i","--input",action = 'store',type = 'string',dest = 'INPUT',help = "")
+(opt, args) = parser.parse_args()
+if opt.INPUT == None:
+    print('Basic usage')
+    print('')
+    print('     python statistics_read.py -i test.fastq.gz')
+    print('')
+    sys.exit()
+
+
+fin = gzip.open(opt.INPUT, 'rt')
+
+qualCount_LIST = [0]*93
+
+readLength_LIST = []
+
+for lineIDX, line in enumerate(fin):
+    if lineIDX%4 == 0:
+        pass
+    elif lineIDX%4 == 1:
+        read = line.rstrip('\n')
+        readLength = len(read)
+        readLength_LIST += [readLength]
+        pass
+    elif lineIDX%4 == 2:
+        pass
+    elif lineIDX%4 == 3:
+        pass
+    else:
+        qual_LIST = line.rstrip('\n')
+        for qual in qual_LIST:
+            qualCount_LIST[ord(qual) - 33] += 1
+
+
+fout = open(opt.INPUT + '.qual', 'w')
+
+for qualIDX, qualCount in enumerate(qualCount_LIST):
+    fout.write('\t'.join([qualIDX, qualCount]) + '\n')
+fout.close()
+
+
+
+readLength_LIST = sorted(readLength_LIST, reverse=True)
+
+readN = len(readLength_LIST)
+
+totalReadLength = sum(readLength_LIST)
+sumReadLength = 0
+
+maxReadLength = max(readLength_LIST)
+minReadLength = min(readLength_LIST)
+
+for readLength in readLength_LIST:
+    sumReadLength += readLength
+    if float(sumReadLength) / totalReadLength > 0.5:
+        N50 = readLength
+        break
+
+print('Number of reads:'  + '\t' + str(readN))
+print('Total bases:'  + '\t' + str(totalReadLength))
+print('Read length N50:'  + '\t' + str(N50))
+print('Min read length:'  + '\t' + str(minReadLength))
+print('Max read length:'  + '\t' + str(maxReadLength))
+
+
+fout = open(opt.INPUT + '.logCount', 'w')
+
+import math
+
+logCount_LIST = [0]*(int(math.log2(maxReadLength)) + 1)
+logSum_LIST = [0]*(int(math.log2(maxReadLength)) + 1)
+
+for readLength in readLength_LIST:
+    logCountIDX = int(math.log2(readLength))
+    logCount_LIST[logCountIDX] += 1
+    logSum_LIST[logCountIDX] += readLength
+
+for logCountIDX, (logCount, logSum) in enumerate(zip(logCount_LIST, logSum_LIST)):
+    fout.write('\t'.join(map(str,[logCountIDX, logCount, logSum])) + '\n')
+
+fout.close()
+
+fout = open(opt.INPUT + '.count', 'w')
+
+count_LIST = [0]*(int((maxReadLength)/1000) + 1)
+sum_LIST   = [0]*(int((maxReadLength)/1000) + 1)
+
+for readLength in readLength_LIST:
+    countIDX = int((readLength)/1000)
+    count_LIST[countIDX] += 1
+    sum_LIST[countIDX] += readLength
+
+for countIDX, (count, sum) in enumerate(zip(count_LIST, sum_LIST)):
+    fout.write('\t'.join(map(str,[countIDX, count, sum])) + '\n')
+
+fout.close()
